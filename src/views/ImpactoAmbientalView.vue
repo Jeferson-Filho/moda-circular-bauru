3cdd21323a9f54769bb374ccd323d5e0f5ef4f7e
Jeferson-Filho <jpdietrichfilho.2015@gmail.com>
2024-10-20 01:49:21 -0300

    :lipstick: fix: adding soundcloud audio

--- src/views/ImpactoAmbientalView.vue
+++ src/views/ImpactoAmbientalView.vue
@@ -11,28 +11,45 @@
                 <p>Ademais, é uma das maiores consumidoras de água. De acordo com a Ellen MacArthur Foundation, a produção de roupas e tecidos consome 93 bilhões de metros cúbicos de água anualmente, o equivalente a 37 milhões de piscinas olímpicas. Desde o cultivo de fibras naturais até o processo de lavagem e tingimento, a quantidade de água utilizada é enorme, não obstante a utilização de produtos químicos na produção contamina fontes de água, afetando a qualidade da água e a vida aquática.</p>
                 <p>Mediante a essa realidade, fica difícil imaginar uma metamorfose considerável, mas ela não é impossível. O novo conceito de Moda Circular, torna-se o principal caminho para transformar o cenário da sociedade do consumo e suas contradições. Ao estimular o reaproveitamento e reduzir o descarte, esses mercados alternativos contribuem diretamente para a preservação ambiental.</p>
             </div>
+            <iframe width="100%"
+                height="300"
+                scrolling="no"
+                frameborder="no"
+                allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1938089780&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            ></iframe>
+            <!-- <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+                <a href="https://soundcloud.com/jefinho-dietrich"
+                    title="Jefinho Dietrich"
+                    target="_blank"
+                    style="color: #cccccc; text-decoration: none;">Jefinho Dietrich</a> · 
+                <a href="https://soundcloud.com/jefinho-dietrich/gravacao_compradora"
+                    title="gravacao_compradora"
+                    target="_blank"
+                    style="color: #cccccc; text-decoration: none;">gravacao_compradora</a>
+            </div> -->
+            <!-- <MediaComponent
+                title="Trilha Sonora"
+                type="audio"
+                source="/audios/gravacao_compradora.mp3"
+                audioType="audio/mp3"
+            /> -->
         </div>
     </section>
-    <MediaComponent
-        title="Trilha Sonora"
-        type="audio"
-        source="/audios/gravacao_compradora.mp3"
-        audioType="audio/mp3"
-    />
     <NextPageComponent page-name="Tipos de moda circular" page-link="/tipos-de-moda" />
 </template>
 
 <script>
 	import BannerComponent from '@/components/BannerComponent.vue';
 	import NextPageComponent from '@/components/NextPageComponent.vue';
-	import MediaComponent from '@/components/MediaComponent.vue';
+	// import MediaComponent from '@/components/MediaComponent.vue';
   
   export default {
     name: 'ImpactoAmbientalView',
     components: {
         BannerComponent,
         NextPageComponent,
-        MediaComponent,
+        // MediaComponent,
     }
 };
 </script>
@@ -42,7 +59,11 @@
 		margin-bottom: 32px;
 	}
 
-	.impacto-ambiental__body p:not(:last-child) {
+	/* .impacto-ambiental__body p:not(:last-child) {
+		margin-bottom: 16px;
+	} */
+
+	.impacto-ambiental__body p {
 		margin-bottom: 16px;
 	}
 </style>
